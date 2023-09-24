8d749b4ab5f428bad352c0efa4177043e39a0281
Aslan <aibadullayev14284@ada.edu.az>
2023-09-24 20:03:33 +0400

    Initial commit done. Initial folders and files created.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientApp
+{
+    public class Program // Developed by Aslan Ibadulla.
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerApp
+{
+    public class Program // Developed by Aslan Ibadulla
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
