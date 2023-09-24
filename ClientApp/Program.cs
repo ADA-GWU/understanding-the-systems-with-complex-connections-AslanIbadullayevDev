7749581ab6c8d9f914d58dbb3a4000f729b5cd09
Aslan <aibadullayev14284@ada.edu.az>
2023-09-25 00:25:42 +0400

    Configurations of client and server ip address, end points and so on created.

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ClientApp
 {
     public class Program // Developed by Aslan Ibadulla.
     {
-        public static void Main(string[] args)
+        public static int Main(String[] args)
         {
-            
+            RunClient();
+            return 0;
+        }
+
+        public static void RunClient()
+        {
+            try
+            {
+                // Configuration of host, ip address of the client starts here.
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 11000); // server's end point.
+
+                // Creation of client's socket and binding it with remote end point
+                // TO DO
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerApp
 {
     public class Program // Developed by Aslan Ibadulla
     {
-        public static void Main(string[] args)
+        public static int Main(String[] args)
         {
-            
+            RunServer(args);
+            return 0;
+        }
+
+        public static void RunServer(String[] args)
+        {
+            // necessary hoisted variables here.
+            byte[] bytes = null; 
+            string data = null; 
+
+            // Configuring host, ip address, localendpoint of server/listener starts from here.
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, Convert.ToInt32(args[0]));
+
+            // Creation of server/ listener socket 
+            // TO DO
         }
     }
 }
