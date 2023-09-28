b788c38160faaaef38fd088a4c1bc121c720aaa6
Aslan <aibadullayev14284@ada.edu.az>
2023-09-28 23:56:05 +0400

    new method for updating the port numbers added.

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -8,46 +8,88 @@ namespace ClientApp
     public class Program // *** Developed by Aslan Ibadullayev ***
     {
         public static int num;
+        public static List<int> ports = new List<int>();
         public static int Main(String[] args)
         {
             RunClient();
             return 0;
         }
 
+        public static void UpdatePortNumbers()
+        {
+            using (TextReader reader = new StreamReader("../PortNumbers.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ports.Add(int.Parse(line));
+                }
+
+                reader.Close();
+            }
+        }
+
         public static void RunClient()
         {
+
+            int serverTurn = 0;
             byte[]? bytes = new byte[1024];
             try
             {
+                using (TextReader reader = new StreamReader("../PortNumbers.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        ports.Add(int.Parse(line));
+                    }
+
+                    reader.Close();
+                }
+
                 // Configuration of host, ip address of the client starts here.
                 IPHostEntry host = Dns.GetHostEntry("localhost");
                 IPAddress ipAddress = host.AddressList[0];
-                IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 9001); // server's end point.
 
-                // Creation of client's socket and binding it with remote end point
-
-                Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                client.Connect(remoteEndPoint);
+   
                 while (true)
                 {
-                    
+                    UpdatePortNumbers();
+                    Console.WriteLine("debugging...");
+                    IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, ports[serverTurn % ports.Count]); // server's end point.
+                    Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    client.Connect(remoteEndPoint);
+
                     bool parseOk = int.TryParse(Console.ReadLine(), out num);
 
-                    if (parseOk) {
+                    if (parseOk)
+                    {
                         bytes = Encoding.ASCII.GetBytes(num.ToString());
                         client.Send(bytes);
                     }
                     else
                     {
-                        Console.WriteLine("Enter a number");
+                        Console.WriteLine("Enter a number!");
                     }
+
+                    if (serverTurn % ports.Count == 0)
+                        serverTurn = 0;
+
+                    serverTurn++;
+                    client.Close();
+                    Console.WriteLine("Success!");
+                    
                 }
-                
             }
             catch (SocketException ex)
             {
                 Console.WriteLine(ex.Message);
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         }
     }
-}
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -7,60 +7,89 @@ namespace ServerApp
 {
     public class Program // *** Developed by Aslan Ibadullayev ***
     {
+        public static List<string> list = new List<string>();
         public static int Main(String[] args)
         {
             RunServer(args);
             return 0;
         }
 
+        public static void UpdatePortNumbers()
+        {
+            using (TextReader reader = new StreamReader(File.Open("../PortNumbers.txt", FileMode.OpenOrCreate)))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                    list.Add(line);
+
+                reader.Close();
+            }
+        }
+
         public static void RunServer(String[] args)
         {
-            // necessary hoisted variables here.
-            
+     
             try
             {
-                // Configuring host, ip address, localendpoint of server/listener starts from here.
+                using (TextReader reader = new StreamReader(File.Open("../PortNumbers.txt", FileMode.OpenOrCreate)))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                        list.Add(line);
+
+                    reader.Close();
+                }
+
+                using (StreamWriter writer = new StreamWriter("../PortNumbers.txt", true))
+                {
+                    if (!list.Contains(args[0]))
+                        writer.WriteLine(args[0]);
+                }
+
+                // Configuring host, ip address, local endpoint of server/listener starts from here.
                 IPHostEntry host = Dns.GetHostEntry("localhost");
                 IPAddress ipAddress = host.AddressList[0];
                 IPEndPoint serverEndPoint = new IPEndPoint(ipAddress, Convert.ToInt32(args[0]));
 
-
-                // Creation of server/ listener socket 
-                // TO DO
                 Socket server = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
                 server.Bind(serverEndPoint);
 
-                server.Listen(10); // server can accept up to 100 requests at a time.
+                server.Listen(10);
 
                 Console.WriteLine("ready to serve!");
                 Console.WriteLine("listening on port {0}", serverEndPoint.Port);
-                Console.WriteLine("Enter stop to quit");
 
-                Socket handler = server.Accept();
                 while (true)
                 {
-                    
+
+                    Socket handler = server.Accept();
+                    Console.WriteLine("Success!");
                     byte[] bytes = new byte[1024];
                     string data = string.Empty;
 
                     int size = handler.Receive(bytes);
                     data = Encoding.ASCII.GetString(bytes, 0, size);
                     Console.WriteLine(int.Parse(data) * 2);
-
+                    UpdatePortNumbers();
                 }
             }
-            catch(SocketException ex)
+            catch (SocketException ex)
             {
-                Console.WriteLine("Socket-related error!" + " " + ex.Message + ex.InnerException);
+                Console.WriteLine("socket-related error!" + " " + ex.Message + ex.InnerException);
             }
-            catch(FormatException)
+            catch (FormatException)
             {
                 Console.WriteLine("enter an integer for server port number!");
             }
             catch (Exception ex)
             {
-                Console.WriteLine("some unknown error");
+                Console.WriteLine("some unknown error " + ex.Message);
             }
+
+        }
         }
     }
-}
+
